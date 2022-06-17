6a64fb933b317579e1e8f051ddc959e2cce7d3e9
Eugeno DIAS <edias@titan.lirmm.fr>
2022-06-17 20:56:48 +0200

    Improving training

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-from regex import F
 import torch
 from torch import optim, nn
 import torch.nn.functional as F
@@ -37,9 +36,18 @@ def train(
     print("Collecting the dataset...")
     # Create a dataset
     dataset = YTDataset(dataset_path, transform=transform, max_imgs_per_class=max_imgs_per_class)
-    '''
+    
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+
     # Split into train and val
-    train_dataset, val_dataset = train_test_split(dataset, test_size=(1 - train_split))
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+    
+    val_size = int(val_size/2)
+    test_size = len(val_dataset) - val_size
+
+    # Split into val and test
+    val_dataset, test_dataset = torch.utils.data.random_split(dataset, [val_size, test_size])
 
     # Split val and test
     val_dataset, test_dataset = train_test_split(val_dataset, test_size=0.5)
@@ -54,17 +62,20 @@ def train(
     test_loader = torch.utils.data.DataLoader(
         test_dataset, batch_size=batch_size, shuffle=True
     )
-    '''
+    
     train_loader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=True
     )
 
 
     # Create a model
-    model = torchvision.models.convnext_base(pretrained=True).to(device=device)
+    model = torchvision.models.convnext_base(pretrained=True)
+
     model.classifier[2] = nn.Linear(
         in_features=model.classifier[2].in_features, out_features=num_classes
     )
+
+    model = model.cuda(device=device)
 
     # Create an optimizer
     optimizer = optim.AdamW(model.parameters(), lr=lr)
@@ -100,13 +111,13 @@ def train(
             optimizer.step()
 
             # Print the loss
-            if i % 100 == 0:
+            if i % 1000 == 0:
                 print(
                     "Epoch: {}, Iteration: {}, Loss: {}".format(
                         epoch, i, loss_value.item()
                     )
                 )
-        '''
+        
         # Running validation to get accuracy
         with torch.no_grad():
             total_loss = 0
@@ -141,8 +152,8 @@ def train(
                     epoch, correct / total, total_loss / total
                 )
             )
-        '''
-    '''
+        
+    
     # Running at the end test to get accuracy
     with torch.no_grad():
         total_loss = 0
@@ -178,16 +189,18 @@ def train(
                 correct / total, total_loss / total
             )
         )
-    '''
+
+        torch.save(model.state_dict(), "./")
+    
 
 if __name__ == "__main__":
     train(
-        batch_size=32,
-        dataset_path="yt_db",
+        batch_size=4,
+        dataset_path="../../yt_db",
         num_classes=6900,
         train_split=0.7,
-        epochs=10,
+        epochs=5,
         lr=1e-4,
-        device="cuda",
+        device="cuda:1",
         max_imgs_per_class=10,
     )
